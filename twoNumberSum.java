3c4d447e0cbfa1193fe0ad10fd3ec0a3d6d7c867
Shabin k <73272797+SHABIN-K@users.noreply.github.com>
2023-08-25 22:59:47 +0530

    Create twoNumberSum.java

---
+++ twoNumberSum.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static int[] twoNumberSum(int [] array, int target){
+        for(int i = 0; i < array.length-1; i++){
+            for (int j = i+1; j < array.length; j++) {
+                if (array[i] + array[j] == target) {
+                    return new int[]{array[i],array[j]};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        int[] array = {6,5,7,9,4,0,2};
+        int target = 10;
+        int[] result = twoNumberSum( array, target);
+        System.out.println("result is :");
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+    }
+}
